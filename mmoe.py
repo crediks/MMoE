73509970f897bf7cf273571988fe5dc4ed6a3df8
北望花村 <44967755+crediks@users.noreply.github.com>
2021-08-12 13:36:21 +0800

    Update mmoe.py

--- mmoe.py
+++ mmoe.py
@@ -5,18 +5,18 @@ class MMoE(object):
         self.num_tasks = num_tasks
 
     def get_output(self, inputs):
-        expert_weight = tf.get_variable(name='expert_weight', initializer=xavier_init, shape=[inputs.get_shape()[-1], self.hidden_size, self.num_experts]           
+        expert_weight = tf.get_variable(name='expert_weight', initializer=xavier_init, shape=[inputs.get_shape()[-1], self.hidden_size, self.num_experts])          
         expert_bias = tf.get_variable(name='expert_bias', initializer=xavier_init, shape=[self.hidden_size, self.num_experts])
         
         expert_output = tf.tensordot(inputs, expert_weight, axes=1) + expert_bias
         expert_output = tf.nn.relu(expert_output, name='expert_output')
 
-        gate_weight = tf.get_variable(name='gate_weight', initializer=xavier_init, shape=[inputs.get_shape()[-1], self.num_experts, self.num_tasks]
+        gate_weight = tf.get_variable(name='gate_weight', initializer=xavier_init, shape=[inputs.get_shape()[-1], self.num_experts, self.num_tasks])
         gate_bias = tf.get_variable(name='gate_bias', initializer=xavier_init, shape=[self.num_experts, self.num_tasks])
         
         gate_output = tf.tensordot(inputs, gate_weight, axes=1) + gate_bias
 
-        gate_outputs = tf.split(gate_output, num_or_size_splits=num_tasks, axis=2)
+        gate_outputs = tf.split(gate_output, num_or_size_splits=self.num_tasks, axis=2)
 
         final_outputs = []
         for gate_output in gate_outputs:
